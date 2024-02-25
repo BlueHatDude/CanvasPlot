5d222feaf3ccc3d3d342f090c689853d9b9eb9db
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-02-25 17:26:45 -0500

    initial commit

---
+++ Canvas.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+public class Canvas {
+
+    final public int WIDTH = 10;
+    final public int HEIGHT = 10;
+    private char[][] canvas = new char[HEIGHT][WIDTH];
+    Coordinate coordinates = new Coordinate(0, 0);
+    
+    Canvas() {
+        coordinates.setConstraints(0, WIDTH - 1, 0, HEIGHT - 1);
+
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                this.canvas[i][j] = '*';
+            }
+        }
+
+    }
+    
+    
+    public void printCanvas() {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                System.out.printf(" %c ", this.canvas[i][j]);
+            }
+            System.out.println();
+        }
+    }    
+
+
+    public void start() {
+        System.out.println("Started");
+       
+        while (true) {
+            char input = getMove();
+
+            if (input == 'q') {
+                break;
+            } else {
+                handleInput(input);
+                System.out.printf("Current Coordinates: (%d, %d)\n",
+                                    this.coordinates.x, this.coordinates.y);
+            }
+
+        }
+
+    }
+
+
+    private char getMove() {
+        char input;
+        @SuppressWarnings("resource")
+        Scanner myScanner = new Scanner(System.in);
+
+        input = myScanner.next().charAt(0);
+
+        return input;
+    }
+
+
+    /**
+     * Commands:
+     * * u: move up
+     * * d: move down
+     * * r: move right
+     * * l: move left
+     * * p: place point
+     * * q: quit
+     * * s: see map
+     * * h: print help
+     */
+    private void handleInput(char inp) {
+
+        switch (inp) {
+            case 'u':
+                coordinates.goUp(1);
+                break;
+            case 'd':
+                coordinates.goDown(1);
+                break;
+            case 'r':
+                coordinates.goRight(1);
+                break;
+            case 'l':
+                coordinates.goLeft(1);
+                break;
+            case 'p':
+                placePoint(this.coordinates.x, this.coordinates.y);
+                break;
+            case 's':
+                this.printCanvas();
+                break;
+            case 'h':
+                printHelp();
+                break;
+
+        }
+
+    }
+
+    private void placePoint(int x, int y) {
+        this.canvas[y][x] = '#';
+    }
+
+    private void printHelp() {
+        String helpString = new String();
+        helpString = """
+            Commands List:
+                u: move up
+                d: move down
+                r: move right
+                l: move left
+                p: place point
+                s: see map
+                h: print help  
+                q: quit     
+                """;
+        System.out.println(helpString);
+    }
+
+}
---
+++ Coordinate.java
@@ -0,0 +1,39 @@
+public class Coordinate {
+
+    public int x;
+    public int y;
+    
+    private int horizontalMax;
+    private int horizontalMin;
+    private int verticalMax;
+    private int verticalMin;
+    
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    final public void setConstraints(int hmin, int hmax, int vmin, int vmax) {
+        this.horizontalMin = hmin;
+        this.horizontalMax = hmax;
+        this.verticalMin = vmin;
+        this.verticalMax = vmax;
+    }
+    
+    final public void goUp(int amount) {
+        this.y = Math.min(this.y + amount, verticalMax);
+    }
+
+    final public void goDown(int amount) {
+        this.y = Math.max(this.y - amount, verticalMin);
+    }
+    
+    final public void goLeft(int amount) {
+        this.x = Math.max(this.x - amount, horizontalMin);
+    }
+    
+    final public void goRight(int amount) {
+        this.x = Math.min(this.x + amount, horizontalMax);
+    }
+
+}
